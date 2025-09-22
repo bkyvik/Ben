d06c91fd014babf7d3e71a83cdb74d388c5a0fd0
Ben Kyvik <benkyvik@berkeley.edu>
2025-09-22 11:58:50 -0700

    done with hw2

---
+++ Homework/homework2/homework2.py
@@ -0,0 +1,69 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+# Git is a system that keeps track of files and repositories, GitHub is a cloud-
+# based service connected to Git repositories, and Git Bash is a software for
+# Windows that allows users to use the terminal in the Git language
+# 2) What’s the difference between the terminal and the command line?
+# The terminal is the text-based interface that allows users to access files,
+# whereas the command line is the line within the terminal that the user types
+# commands into
+# 3) How does Windows PowerShell differ from Git Bash?
+# PowerShell is the native terminal interface for the Windows operating system,
+# whereas Git Bash is a downloadable software that emulates the Git interface
+# as it appears in other operating systems
+# 4) What’s the difference between Anaconda, conda, and Python?
+# Anaconda is used to manage python and conda, conda is a package manager
+# compatible with python, and Python is a programming language
+# 5) What is VS Code?
+# Visual studio code is a code editor developed by Microsoft
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+# Jupyter Notebook is a web-based computing interface.  It is different from Jupyter
+# Lab in that Notebook is used for single documents while Lab can handle larger
+# systems
+# 7) What does ~/ mean?
+# Home directory
+# 8) What’s the difference between an absolute path and a relative path?
+# An absolute path locates a file within the full context of its home directory
+# whereas a relative path only locates files in the context of the working directory
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+# Absolute: ~/benky/python_decal_fa25/Ben/course_assignments/homework2
+# Relative: Ben/course_assignments/homework2
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd ..
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+# removes current directory
+# 12) What do the following commands do?
+# git add
+# stages changes to be saved
+# git commit
+# records changes
+# git push
+# uploads local repository to the cloud
+# 13) What's the difference between "git add ." and "git add <file>"?
+# git add . stages everything for saving, whereas git add <file> just stages
+# select files
+# 14) What do "git status" and "git log -1" do?
+# git status tells status of current repository and git log shows most recent
+# version
+# 15) What’s the difference between cloning a repository and pulling from it?
+# cloning a repository makes a copy of the whole thing, whereas pulling from it
+# just takes certain parts
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+# I guess just setting up Git Bash on my terminal was a little confusing for me.
+# I've been going to office hours everyday for help catching up :)
+# 17) What’s a question you still have? What’s something you’re confused about?
+# Still not completely sure I understand the purposes of git add, commit, and
+# push yet
+# 18) Tell me a fun fact!
+# During the American Revolution, North and South Florida were the two English
+# colonies that remained loyal to the crown.  They were conquered by France in 1791
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(5 // 2)
+# performs floor division
